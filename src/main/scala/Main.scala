de32fb2d9e18bde267900955c76993bb1c2a41b7
fujidana <fujidanawt@gmail.com>
2021-10-24 12:55:47 +0900

    add block test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,11 +4,9 @@ import scala.util.parsing.combinator._
 
 
 object SurveyParser extends RegexParsers {
-  def section = """\section{""" ~ """[^\}]*""".r ~ """}""" ^^ {
-    case x ~ y ~ z => x + y + z
-  }
+  def section = """\\section\{[\s\S]*\}""".r
 
-  def allSpace = """[\s]*""".r ^^ (_ => "space")
+  def allSpace = """[\s]*""".r ^^ (_ => "\n")
   //def frame = """\begin{frame}""" ~ allSpace ~ blocks ~ allSpace ~ """\end{frame}""" //rep(frame)
   //def blockCount = 3
   //def blocks = block(1) ~ block(2) ~ block3 ^^ {
@@ -18,10 +16,8 @@ object SurveyParser extends RegexParsers {
   //// section, subsectionがなくてもframe, blockが有効なことの確認
   //// blockのタイトル無しの場合の確認
   //// section, subsectionのパーサをframe, blockのパーサと交換
-  //def block(n: Int) =
-  //  subsection((title(n))) ~ raw"[^(\\subsection\{${title(n + 1)}\})]*".r ^^ {
-  //    case mark ~ body => mark + body
-  //  }
+  def block =
+     """\\begin\{block\}\{1\}[\s\S]*\\end\{block\}""".r 
   //def block3 = subsection(title(3))
   ////def subsection1 = raw"\\subsection\{$title\}".r
   //def subsection(title: String) = """\subsection{""" ~ title ~  """}""" ^^ {
@@ -39,6 +35,10 @@ object SurveyParser extends RegexParsers {
 object Main {
   import SurveyParser._
   def main(args: Array[String]): Unit = {
-    println(parseAll(section, """\section{Hello}""").get)
+    val input = """\begin{block}{1}
+      Hello World
+    \end{block}"""
+    val output = parseAll(block, input).get
+    println(output)
   }
 }
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -12,4 +12,24 @@ class SurveyParserTest extends AnyFunSuite {
     val correct = """\section{Hello}"""
     assert(output === correct)
   }
+
+  test("allSpace test") {
+    val input = """    
+           
+    """
+    val output = parseAll(allSpace, input).get
+    val correct = "\n"
+    assert(output === correct)
+  }
+
+  test("block test") {
+    val input = """\begin{block}{1}
+      Hello World 
+    \end{block}"""
+    val output = parseAll(block, input).get
+    println(input)
+    println(output)
+    val correct = input
+    assert(output === correct)
+  }
 }
